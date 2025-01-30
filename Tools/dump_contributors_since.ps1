739fcba3b4328d183e8acc63e5bd38b6ec82241d
Sh18RW <sh18rw@yandex.ru>
2025-01-30 14:55:57 +0300

    Contributors adding update.

--- Tools/dump_contributors_since.ps1
+++ Tools/dump_contributors_since.ps1
@@ -13,8 +13,9 @@ $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 $engine = & "$PSScriptRoot\dump_commits_since.ps1" -repo space-wizards/RobustToolbox -since $since -until $until
 $content = & "$PSScriptRoot\dump_commits_since.ps1" -repo space-wizards/space-station-14 -since $since -until $until
+$contentBF = & "$PSScriptRoot\dump_commits_since.ps1" -repo Breaking-Facility/space-station-14 -since $since -until $until
 
-$contribs = ($content + $engine) `
+$contribs = ($content + $engine + $contentBF) `
     | Select-Object -ExpandProperty author `
     | Select-Object -ExpandProperty login -Unique `
     | Where-Object { -not $ignore[$_] }`
