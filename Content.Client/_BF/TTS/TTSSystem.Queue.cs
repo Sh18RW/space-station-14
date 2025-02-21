b24dd1f2c2e2cd077d0f962bb43d130f6b1ca702
Sh18RW <sh18rw@yandex.ru>
2025-02-22 00:16:03 +0300

    Make global audio queue.

--- Content.Client/_BF/TTS/TTSSystem.Queue.cs
+++ Content.Client/_BF/TTS/TTSSystem.Queue.cs
@@ -11,9 +11,21 @@ public partial class TTSSystem
 {
     [Dependency] private readonly IGameTiming _timing = default!;
 
+    // TODO: make it with normal station announcement system.
+    private readonly Queue<PlayTTSAudioData> _publicAudioQueue = [];
+    private TimeSpan _endTime = TimeSpan.Zero;
+
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
+
+        if (_endTime > _timing.CurTime)
+        {
+            if (_publicAudioQueue.TryDequeue(out var audioData))
+            {
+                PlayGlobalTTSAudio(audioData);
+            }
+        }
 
         var queue = EntityQueryEnumerator<TTSComponent>();
 
@@ -31,8 +43,14 @@ public partial class TTSSystem
         }
     }
 
-    private void QueueAudio(EntityUid source, PlayTTSAudioData data)
+    private void QueueAudio(EntityUid? source, PlayTTSAudioData data)
     {
+        if (source == null)
+        {
+            _sawmill.Verbose($"Add to queue public TTS audio.");
+            _publicAudioQueue.Enqueue(data);
+            return;
+        }
         if (!TryComp<TTSComponent>(source, out var ttsComponent))
         {
             _sawmill.Warning($"Ignore TTS on {source} because it doesn't have TTS component!");
@@ -56,6 +74,22 @@ public partial class TTSSystem
 
         component.EndTime = _timing.CurTime + audioResource.AudioStream.Length;
         _audio.PlayEntity(audioResource.AudioStream, source, audioParams);
+
+        _contentRoot.RemoveFile(data.Path);
+    }
+
+    // ReSharper disable once InconsistentNaming
+    private void PlayGlobalTTSAudio(PlayTTSAudioData data)
+    {
+        var audioResource = new AudioResource();
+        audioResource.Load(IoCManager.Instance!, Prefix / data.Path);
+
+        var audioParams = AudioParams.Default
+            .WithVolume(AdjustVolume(data.Effects.HasFlag(TTSEffects.Whisper)))
+            .WithMaxDistance(AdjustDistance(data.Effects.HasFlag(TTSEffects.Whisper)));
+
+        _endTime = _timing.CurTime + audioResource.AudioStream.Length;
+        _audio.PlayGlobal(audioResource.AudioStream, audioParams);
 
         _contentRoot.RemoveFile(data.Path);
     }
