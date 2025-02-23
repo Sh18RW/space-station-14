9c642f85cd9b62f8820643ebc44a8da7a6a1b07a
Sh18RW <sh18rw@yandex.ru>
2025-02-23 16:24:35 +0300

    Add dummy discord bans notification.

---
+++ Content.Shared/_CP/CCVars/CPCCVars.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._CP.CCVars;
+
+/// <summary>
+/// CP CCVars.
+/// </summary>
+[CVarDefs]
+// ReSharper disable once InconsistentNaming
+public sealed partial class CPCCVars
+{
+}
